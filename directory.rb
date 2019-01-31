dc409f30e57d811331b4a22aa4de5177c8d4bdfd
anu <anu_iv2002@hotmail.com>
2019-01-31 13:51:43 +0000

    Loading students data

--- directory.rb
+++ directory.rb
@@ -1,4 +1,36 @@
 @students = [] # an empty array accessible to all methods
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" #to add more items
+end
+
+def interactive_menu
+loop do
+  print_menu
+  process(gets.chomp)
+ end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "No selection made"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -34,40 +66,10 @@ def input_students
   #return the array of students
 end
 
-
-def interactive_menu
-loop do
-  print_menu
-  process(gets.chomp)
- end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit" #to add more items
-end
-
 def show_students
   print_header
   print_students_list
   print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "9"
-    exit
-  else
-    puts "No selection made"
-  end
 end
 
 def print_header
@@ -124,6 +126,16 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+  @students << {name: name, cohort: cohort.to_sym}
+end
+file.close
+end
+
 =begin
 def interactive_menu
   students = []
